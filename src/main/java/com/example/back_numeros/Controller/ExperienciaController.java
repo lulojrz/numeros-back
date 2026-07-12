b7dae904d36c73dc1988d3cec0b794efe57e6734
lulojrz <lulojrz18@gmail.com>
2026-07-12 18:36:33 -0300

    corrigiendo error 2.0

--- src/main/java/com/example/back_numeros/Controller/ExperienciaController.java
+++ src/main/java/com/example/back_numeros/Controller/ExperienciaController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("experiencias")
+@RequestMapping("/experiencias")
 @CrossOrigin(origins = "https://colegiales.netlify.app")
 public class ExperienciaController {
     @Autowired
