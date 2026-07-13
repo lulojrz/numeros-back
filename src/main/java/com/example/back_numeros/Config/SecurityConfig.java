51dbf34924104b028e4177de6fcc1b9cfedb0654
lulojrz <lulojrz18@gmail.com>
2026-07-13 20:33:46 -0300

    arreglando error de publicador

--- src/main/java/com/example/back_numeros/Config/SecurityConfig.java
+++ src/main/java/com/example/back_numeros/Config/SecurityConfig.java
@@ -34,8 +34,9 @@ public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Excepti
             .authorizeHttpRequests(auth -> auth
                     .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                     .requestMatchers("/usuarios/login", "/usuarios/cambiar-contrasena", "/api/numeros","/usuarios","/experiencias/traer","/experiencias/agregar").permitAll()
-                    .requestMatchers("/usuarios/editar/{id}", "/usuarios/borrar/{id}").hasAuthority("ROLE_ANC")
-                    .requestMatchers( "/usuarios/crear", "/api/editar/{id}", "/api/borrar/{id}", "/api/agregar").hasAnyAuthority("ROLE_ANC", "ROLE_SM")
+                    .requestMatchers("/api/editar/{id}").authenticated()
+                    .requestMatchers( "/usuarios/borrar/{id}").hasAuthority("ROLE_ANC")
+                    .requestMatchers( "/usuarios/crear", "/api/borrar/{id}", "/api/agregar").hasAnyAuthority("ROLE_ANC", "ROLE_SM")
                     .anyRequest().authenticated()
             );
             
