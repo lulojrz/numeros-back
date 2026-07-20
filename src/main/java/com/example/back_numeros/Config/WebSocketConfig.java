fa7249081db96f505315d5a6858ef1266b949772
lulojrz <lulojrz18@gmail.com>
2026-07-20 19:58:25 -0300

    optimizando el rendimiento de la pagina

--- src/main/java/com/example/back_numeros/Config/WebSocketConfig.java
+++ src/main/java/com/example/back_numeros/Config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.example.back_numeros.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -12,13 +13,24 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic"); // El canal al que se suscribirá React
+        // Configuramos un scheduler para habilitar el Heartbeat
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(1);
+        taskScheduler.setThreadNamePrefix("wss-heartbeat-thread-");
+        taskScheduler.initialize();
+
+        config.enableSimpleBroker("/topic")
+                .setHeartbeatValue(new long[]{10000, 10000}) // 10 segundos de latido
+                .setTaskScheduler(taskScheduler);
+                
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // El endpoint al que React se conectará inicialmente
-        registry.addEndpoint("/ws-turnos").setAllowedOriginPatterns("*").withSockJS();
+        // El endpoint al que React se conectará inicialmente, con CORS limitado al entorno real
+        registry.addEndpoint("/ws-turnos")
+                .setAllowedOriginPatterns("https://colegiales.netlify.app")
+                .withSockJS();
     }
 }
